5950b0ccc995701b381185c68fa9a726afb2862a
fkaexe <josemiguelmartinezay@gmail.com>
2026-01-15 19:14:41 +0100

    Configuración de componentes, interface y rutas.

---
+++ frontend_flickr/src/app/services/flickr-services.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+import { IImage, ISearchResponse } from '../interfaces/interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+  private apiUrl = 'http://localhost:8080/api/images';
+
+  constructor(private http: HttpClient) { }
+
+  searchImages(query: string, page: number = 1, size: number = 20): Promise<ISearchResponse> {
+    const params = new HttpParams()
+      .set('query', query)
+      .set('page', page.toString())
+      .set('size', size.toString());
+
+    return firstValueFrom(this.http.get<ISearchResponse>(`${this.apiUrl}/search`, { params }));
+  }
+
+  getImageDetail(id: string): Promise<IImage> {
+    return firstValueFrom(this.http.get<IImage>(`${this.apiUrl}/${id}`));
+  }
+}
