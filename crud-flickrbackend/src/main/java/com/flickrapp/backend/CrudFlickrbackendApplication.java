ea43259d2fadaa6baf80250b0717ac59e65fed60
fkaexe <josemiguelmartinezay@gmail.com>
2026-01-15 09:23:06 +0100

    Creación del proyecto de Spring

---
+++ crud-flickrbackend/src/main/java/com/flickrapp/backend/CrudFlickrbackendApplication.java
@@ -0,0 +1,13 @@
+package com.flickrapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudFlickrbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudFlickrbackendApplication.class, args);
+	}
+
+}
