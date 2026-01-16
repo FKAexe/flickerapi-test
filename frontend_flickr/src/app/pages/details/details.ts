18b5d0ef0062452c25f7603ab465e0b271195093
fkaexe <josemiguelmartinezay@gmail.com>
2026-01-16 11:22:00 +0100

    Maquetación detallada en search y details

--- frontend_flickr/src/app/pages/details/details.ts
+++ frontend_flickr/src/app/pages/details/details.ts
@@ -2,10 +2,10 @@ import { Component, inject, Input } from '@angular/core';
 import {FlickrService} from '../../services/flickr-services';
 import {IImage} from '../../interfaces/interfaces';
 import {RouterLink} from '@angular/router';
-
+import {MATERIAL_MODULES} from '../../material-imports';
 @Component({
   selector: 'app-details',
-  imports: [RouterLink],
+  imports: [RouterLink, ...MATERIAL_MODULES],
   templateUrl: './details.html',
   styleUrl: './details.css',
 })
--- frontend_flickr/src/app/services/flickr-services.ts
+++ frontend_flickr/src/app/services/flickr-services.ts
@@ -10,12 +10,12 @@ export class FlickrService {
   private apiUrl = 'http://localhost:8080/api/images';
 
   constructor(private http: HttpClient) { }
-
+/* Metodo de buscar imágenes */
   searchImages(query: string, page: number = 1, size: number = 20): Promise<ISearchResponse> {
     const params = new HttpParams().set('query', query).set('page', page.toString()).set('size', size.toString());
     return firstValueFrom(this.http.get<ISearchResponse>(`${this.apiUrl}/search`, { params }));
   }
-
+/* Metodo de detalle imágenes*/
   getImageDetail(id: string): Promise<IImage> {
     return firstValueFrom(this.http.get<IImage>(`${this.apiUrl}/${id}`));
   }
