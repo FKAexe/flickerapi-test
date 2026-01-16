d53045f7c256efd54b72f2a185eea64fef78ab7d
fkaexe <josemiguelmartinezay@gmail.com>
2026-01-16 10:08:09 +0100

    Componente detalles recibe datos de la API

--- frontend_flickr/src/app/pages/details/details.ts
+++ frontend_flickr/src/app/pages/details/details.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import {FlickrService} from '../../services/flickr-services';
+import {IImage} from '../../interfaces/interfaces';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './details.html',
   styleUrl: './details.css',
 })
 export class Details {
 
+  @Input() id: string = "";
+  image!: IImage;
+  flickrService = inject(FlickrService);
+
+  async ngOnInit() {
+    const imageId = this.id;
+    const response = await this.flickrService.getImageDetail(imageId);
+    if(!response) {
+      console.log('Image not found');
+    }
+    this.image = response!;
+  } 
 }
